1f4cfd3afbb2b6eed024fcd4bc271418ef3219fd
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-07-31 23:13:35 +0300

    app.js changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  const blog = { id: 1, title: "Blog title", description: "Blog description" };
   res.send(blog);
 });
 

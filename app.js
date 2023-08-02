faece44ec3f1b7ba7904f14e97254d1337a2cdf7
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-08-02 13:59:44 +0300

    Clean Blog 2. bolum eklemeleri yapildi

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+//TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
+
+//MİDDLEWARE
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 app.listen(port, () => {

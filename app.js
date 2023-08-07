d08df2f2e18b1951fd281a56f2896ab99f313417
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-08-07 23:38:47 +0300

    models added

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Post = require('./models/Post');
 
 const app = express();
 const port = 3000;
 
-//TEMPLATE ENGİNE
+//connect db
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-//MİDDLEWARE
+//MİDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -21,8 +34,12 @@ app.get('/about', (req, res) => {
 app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
-app.get('/post', (req, res) => {
-  res.render('post');
+// app.get('/post', (req, res) => {
+//   res.render('post');
+// });
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
